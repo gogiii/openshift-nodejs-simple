15d0e907bc8007b094ebef1f313ea281b880a0f2
gvm <gvm@nm.ru>
2017-08-26 18:44:52 +0300

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # openshift-nodejs-simple
+
+Simple nodejs/express web server to host something on your openshift without rebuilds and git commits.
---
+++ server.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var fs      = require('fs');
+var app     = express();
+
+var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
+
+app.use(express.static('public'));
+
+// error handling
+app.use(function(err, req, res, next){
+  console.error(err.stack);
+  res.status(500).send('Something bad happened!');
+});
+
+app.listen(port, ip);
+console.log('Server running on http://%s:%s', ip, port);
+
+module.exports = app;
